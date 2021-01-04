e25725c2b82368c486970df222dcb6e77cd7ab88
moi <moiling1996@gmail.com>
2021-01-04 11:28:22 +0800

    add fusion(forget add).

--- networks/matting_net.py
+++ networks/matting_net.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
 
+from networks.fnet.fusionnet import FusionNet
 from networks.mnet.dimnet import DIMNet
+from networks.tnet.mobilenet.wrapper import MobileNetWrapper
 from networks.tnet.pspnet import PSPNet
 
 
@@ -10,20 +12,22 @@ class MattingNet(nn.Module):
         super().__init__()
         self.tnet = PSPNet()
         self.mnet = DIMNet()
+        self.fnet = FusionNet()
 
     def forward(self, img, trimap_3=None):
         """
         :param img:
         :param trimap_3: 3 channels.
         """
-        pred_trimap_prob = self.tnet(img)                          # [B, C(BUF=3),     H, W]
-        pred_trimap_softmax = pred_trimap_prob.softmax(dim=1)      # [B, C(BUF=3),     H, W]
+        pred_trimap_prob = self.tnet(img)                                 # [B, C(BUF=3),     H, W]
+        pred_trimap_softmax = pred_trimap_prob.softmax(dim=1)             # [B, C(BUF=3),     H, W]
 
         if trimap_3 is None:
-            concat = torch.cat([img, pred_trimap_softmax], dim=1)  # [B, C(RGB+BUF=6), H, W]
+            concat = torch.cat([img, pred_trimap_softmax], dim=1)         # [B, C(RGB+BUF=6), H, W]
         else:
-            concat = torch.cat([img, trimap_3], dim=1)             # [B, C(RGB+BUF=6), H, W]
+            concat = torch.cat([img, trimap_3], dim=1)                    # [B, C(RGB+BUF=6), H, W]
 
-        pred_matte = self.mnet(concat)                             # [B, C(alpha=1),   H, W]
+        pred_matte_u = self.mnet(concat)                                  # [B, C(alpha=1),   H, W]
+        pred_matte = self.fnet(torch.cat([concat, pred_matte_u], dim=1))  # [B, C(alpha=1),   H, W])
 
-        return pred_matte, pred_trimap_prob
+        return pred_matte, pred_trimap_prob, pred_matte_u
--- train.bat
+++ train.bat
@@ -1,18 +1,22 @@
 @echo off
 
-set img=D:/Dataset/AiSegment/matting_human_half/pre-12/img
-set tmp=D:/Dataset/AiSegment/matting_human_half/pre/trimap
-set mat=D:/Dataset/AiSegment/matting_human_half/pre-12/FBA/alpha
+set img=D:/Dataset/Adobe_Half_Human/comp/image
+set tmp=D:/Dataset/Adobe_Half_Human/comp/trimap
+set mat=D:/Dataset/Adobe_Half_Human/comp/alpha
+set fg=D:/Dataset/Adobe_Half_Human/comp/fg
+set bg=D:/Dataset/Adobe_Half_Human/comp/bg
 set val_out=data/val
 set val_img=D:/Mission/photos
 set val_tmp=D:/Mission/removebg_trimap
 set val_mat=D:/Mission/removebg_alpha
 set ckpt=checkpoints
-set patch_size=60
+set patch_size=200
 set sample=1000
+set epoch=10
 
-:: python train.py -dgr -m=t-net   --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample%
-:: python train.py -dgr -m=m-net   --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample%
-python train.py -dgr -m=end2end --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample%
+:: python train.py -dgr -m=t-net   --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample% --epoch=%epoch%
+python train.py -dgr -m=m-net   --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample% --epoch=%epoch%
+python train.py -dgr -m=f-net   --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample% --epoch=%epoch%
+python train.py -dgr -m=end2end --img=%img% --trimap=%tmp% --matte=%mat% --val-out=%val_out% --val-img=%val_img% --val-trimap=%val_tmp% --val-matte=%val_mat% --ckpt=%ckpt% --patch-size=%patch_size% --sample=%sample% --epoch=%epoch%
 
 Pause
